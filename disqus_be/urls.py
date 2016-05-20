09fa9202eed1fb20f1a42a809e0ffa894333eb9c
Nurken <nurken.otegen@ce.sdu.edu.kz>
2016-05-20 22:43:24 +0600

    added admin urls

--- disqus_be/urls.py
+++ disqus_be/urls.py
@@ -3,8 +3,12 @@
 Include your API resources and views into urlpatterns
 """
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from disqus_be.core_app.api import CommentResource
 
+comment_resource = CommentResource()
 
 urlpatterns = patterns('',
-	(r'^api/', include(comment_resource.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(comment_resource.urls)),
 )
